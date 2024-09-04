da4a783e53db4346931aaa5fda056b8958d7119f
omkarph1 <phadtareomkar3010@gmail.com>
2024-09-04 08:40:58 +0530

    commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import "./Navbar.scss"
 
 function Navbar(){
+    const [open,setOpen] =useState(false)
     return(
         <nav>
             <div className="left">
@@ -16,6 +18,17 @@ function Navbar(){
             <div className="right">
                 <a href="/">Sign In</a>
                 <a href="/" className="register">Sign Up</a>
+                <div className="menuIcon">
+                    <img src="menu.png" alt="" onClick={() => setOpen((prev) => !prev)}/>
+                </div>
+                <div className={open ? "menu active" : "menu"}>
+                <a href="/">Home</a>
+                <a href="/">About</a>
+                <a href="/">Contacts</a>
+                <a href="/">Agents</a>
+                <a href="/">Sign In</a>
+                <a href="/">Sign Up</a>
+                </div>
             </div>
         </nav>
     )
